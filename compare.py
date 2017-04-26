40b24bf152deda89b515029d2b5c1fc0445c32c7
Chrissy Wainwright <chrissy@sixfeetup.com>
2017-04-25 22:07:55 -0500

    initial commit

---
+++ compare.py
@@ -0,0 +1,54 @@
+"""pass two files as arguments
+   $ python compare.py file1 file2
+   script will output version differences
+   between the two files
+"""
+
+import re
+from sys import argv
+
+script, versions1, versions2 = argv
+
+
+def filter_products(file):
+    """ clean up the file so we end up with a list
+        of the products and their versions
+    """
+    f = open(file)
+    lines = (line.rstrip() for line in f)
+
+    versions = {}
+    write = False
+    for line in [l for l in lines if l]:
+        if write is False and line != 'sys.path[0:0] = [':
+            continue
+        elif line.strip() == ']':
+            write = False
+            continue
+        else:
+            write = True
+        line = re.sub('(.*[\/])', '', line)
+        line = re.sub('(-py.*$)', '', line)
+        vkey = re.sub('(\-.*$)', '', line)
+        vval = re.sub('(.*[\-])', '', line)
+        versions[vkey] = vval
+    return versions
+
+f1v = filter_products(versions1)
+f2v = filter_products(versions2)
+
+output = ''
+
+for product in f1v:
+    if product not in f2v:
+        output += "{0} > N/A\n".format(product)
+        continue
+    if f1v[product] == f2v[product]:
+        continue
+    output += "{0} {1} > {2}\n".format(product, f1v[product], f2v[product])
+
+for product in f2v:
+    if product not in f1v:
+        output += "N/A > {0}\n".format(product)
+
+print output
